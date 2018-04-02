864e9bf85f710c28ecab889b641cd1abbe9559cc
William Doody <wdoody@yahoo.com>
2018-04-02 14:51:41 -0600

    ss

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
-$.ajax({
-  // What kind of request
-  method: 'GET',
+// $.ajax({
+//   // What kind of request
+//   method: 'GET',
 
-  // The URL for the request
-  url: 'https://kickflip-api.herokuapp.com/tricks',
+//   // The URL for the request
+//   url: 'https://kickflip-api.herokuapp.com/tricks',
 
-  // The type of data we want back
-  dataType: 'json',
+//   // The type of data we want back
+//   dataType: 'json',
 
-  // Code to run if the request succeeds; the JSON
-  // response is passed to the function as an argument.
-  success: onSuccess
+//   // Code to run if the request succeeds; the JSON
+//   // response is passed to the function as an argument.
+//   success: onSuccess
+// });
+
+// // defining the callback function that will happen
+// // if the request succeeds.
+// function onSuccess(responseData) {
+//     console.log(responseData[3]);
+//     // celebrate!
+// };
+
+$.get("https://kickflip-api.herokuapp.com/tricks", (response) => {
+  processResponse(response[3]);
 });
-
-// defining the callback function that will happen
-// if the request succeeds.
-function onSuccess(responseData) {
-    console.log(responseData);
-    // celebrate!
-};
